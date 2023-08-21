332f0c4a6971f4cc257a7d53f46d0aba6aeb8562
indrajeet790 <indrajeetkumarnayak3@gmail.com>
2023-08-21 17:30:27 +0530

    taking input from user

---
+++ Array/inputOfArray.java
@@ -0,0 +1,24 @@
+package Array;
+
+import java.util.Scanner;
+
+public class inputOfArray {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        // declaring a array
+        int[] arr = new int[10];
+        // taking input from user
+        for (int i = 0; i < n; i++) {
+            System.out.println("enter your element" + i + "th index");
+            arr[i] = sc.nextInt();
+        }
+
+        // printing the user value
+        for (int i = 0; i < n; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+
+}
